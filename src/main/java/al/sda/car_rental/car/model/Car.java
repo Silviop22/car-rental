2d9403ae5213c58a8e2c53aee01af7636c30a74c
silvio-progonati <silvioprogonati@gmail.com>
2024-07-14 13:36:19 +0200

    Initial commit

---
+++ src/main/java/al/sda/car_rental/car/model/Car.java
@@ -0,0 +1,15 @@
+package al.sda.car_rental.car.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
